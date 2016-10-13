d2e80ee7c5e774acfa338cb65c08c918d16284ef
appledix <dxl2010@mail.ru>
2016-10-13 15:44:35 +0300

    Внесены изменения.

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,70 +1,40 @@
-import sys
+import argparse
+import collections
+import re
+import os
 
-def read_console_args():
-    return sys.argv[1:]
 
 def load_data(filepath):
-    data = ""
-    text_file = open(filepath, "r")
-    data = text_file.read()
-    text_file.close()
+    with open(filepath, 'r') as text_file:
+        data = text_file.read()
     return data
 
-def text_to_words_array(text):
-    text = text.lower().replace("\n", " ").replace("\t", " ").split(" ")
-    words_array = []
-    current_word = ""
-    for word in text:
-        current_word = ""
-        for char in word:
-            if char.isalpha() or char == "-":
-                current_word += char
-            else:
-                continue
-        if current_word != "":
-            words_array.append(current_word)
-    return words_array
-
-def get_unique_words(words_array):
-    words_dict = {}
-    for word in words_array:
-        if word in words_dict:
-            words_dict[word] += 1
-        else:
-            words_dict[word] = 1
-    return words_dict
-
+def get_words_from_text(text):
+    words = re.findall(r'\w+', text)
+    return [word.lower() for word in words]
 
 def get_most_frequent_words(text):
-    words_array = text_to_words_array(text)
-    unique_words = get_unique_words(words_array)
-    most_frequent_words = []
-    for i in range(10):
-        most_frequent_word = {"name":None, "frequency":0}   
-        for word in unique_words:
-            current_frequency = unique_words[word]
-            if current_frequency > most_frequent_word["frequency"]:
-                most_frequent_word["name"] = word
-                most_frequent_word["frequency"] = current_frequency
-            else:
-                continue
-        del unique_words[most_frequent_word["name"]]
-        most_frequent_words.append(most_frequent_word["name"])
-    return most_frequent_words
+    words = get_words_from_text(text)
+    number_of_words = 10
+    return collections.Counter(words).most_common(number_of_words)
+
+def filepath_is_valid(filepath):
+    if os.path.isfile(filepath) and filepath.endswith(".txt"):
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
-    parameters = read_console_args()
-    try:
-        if len(parameters) != 1:
-            raise Exception
-        filepath = parameters[0]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filepath", help="Адрес текстового файла.", type=str)
+    args = parser.parse_args()
+    filepath = args.filepath
+    
+    if filepath_is_valid(filepath):
         data = load_data(filepath)
-    except Exception:
-        print\
-        ("Для работы скрипта необходимо передать ему валидный адрес текстового файла.")
-    else:
         most_frequent_words = get_most_frequent_words(data)
-        print\
-        ("Десять наиболее часто используемых в данном тексте слов в порядке убывания:\n",\
-            most_frequent_words)
+        print("Десять наиболее часто используемых в данном " + \
+            "тексте слов в порядке убывания:\n", most_frequent_words)
+    else:
+        print("Некорректные входные данные!")
